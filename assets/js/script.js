97c9c86185eb488d7079f39a14e1ae4264ca50a2
Haroon <mharoonbutt13@gmail.com>
2023-01-21 08:22:09 +0000

    colors and dynamic hours added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
 var currentDay = $("#currentDay");
 var timeBlocks = $("#timeBlocks");
-var currentHour = moment().format("hh");
+var currentHour = moment().format("HH");
 console.log(currentHour);
-var workHours = [9,10,11,12,1,2,3,4,5];
+var workHours = [8,9,10,11,12,13,14,15,16,17];
 let row='';
 for (let i = 0; i< workHours.length; i++) {
     row='';
+    var AmOrPm = workHours[i] >= 12 ? 'PM' : 'AM';
+    var hour = (workHours[i] % 12) || 12; 
     if (currentHour > workHours[i]) {
-        row = "<tr class='p-3' style='background-color: green;'><td>"+ workHours[i] +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
+        row = "<tr class='p-3' style='background-color: green;'><td>"+ hour + AmOrPm +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
     }
-    else if(currentHour === workHours[i]){
-        row = "<tr class='p-3' style='background-color: red;'><td>"+ workHours[i] +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
+    else if(currentHour == workHours[i]){
+        row = "<tr class='p-3' style='background-color: red;'><td>"+ hour + AmOrPm +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
     }
     else{
-        row = "<tr class='p-3' style='background-color: grey;'><td>"+ workHours[i] +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
+        row = "<tr class='p-3' style='background-color: grey;'><td>"+  hour + AmOrPm +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
     }
     $("#timeBlocks tbody").append(row);
     }

a62dcb00ff577de312b9e13ed2e154e6b41d0e05
Haroon <mharoonbutt13@gmail.com>
2023-01-21 11:20:39 +0000

    values getting from localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,103 @@
 var currentDay = $("#currentDay");
-var timeBlocks = $("#timeBlocks");
+var dailyPlanner = $("#timeBlocks");
 var currentHour = moment().format("HH");
-console.log(currentHour);
+// console.log(currentHour);
 var workHours = [8,9,10,11,12,13,14,15,16,17];
+var prevValues = [];
 let row='';
-for (let i = 0; i< workHours.length; i++) {
-    row='';
-    var AmOrPm = workHours[i] >= 12 ? 'PM' : 'AM';
-    var hour = (workHours[i] % 12) || 12; 
-    if (currentHour > workHours[i]) {
-        row = "<tr class='p-3' style='background-color: green;'><td>"+ hour + AmOrPm +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
-    }
-    else if(currentHour == workHours[i]){
-        row = "<tr class='p-3' style='background-color: red;'><td>"+ hour + AmOrPm +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
-    }
-    else{
-        row = "<tr class='p-3' style='background-color: grey;'><td>"+  hour + AmOrPm +"</td><td></td><td><i class='fa fa-save'></i></td></tr>";
-    }
-    $("#timeBlocks tbody").append(row);
-    }
+prevValues = JSON.parse(localStorage.getItem('values'));
 
 
+for (let i = 0; i < workHours.length; i++) {
+    // const element = array[i];
+        var AmOrPm = workHours[i] >= 12 ? 'PM' : 'AM';
+        var hour = (workHours[i] % 12) || 12;
+        // console.log(prevValues);
+        if (prevValues) {
+            row = printDailyPlannerData(hour, prevValues[i]);  
+            dailyPlanner.append(row); 
+        }
+        else{
+            row = printDailyPlannerData(hour,' ');
+            dailyPlanner.append(row);
+        }
+        // printDailyPlannerData(hour,'abc');
+    
+}
+// for (let i = 0; i< workHours.length; i++) {
+//     row='';
+//     var AmOrPm = workHours[i] >= 12 ? 'PM' : 'AM';
+//     var hour = (workHours[i] % 12) || 12; 
+//     if (currentHour > workHours[i]) {
+//         row = "<tr class='p-3' style='background-color: green;'><td>"+ hour + AmOrPm +"</td>"+
+//         "<td></td>"+
+//         "<td><i class='fa fa-save'></i></td></tr>";
+//     }
+//     else if(currentHour == workHours[i]){
+//         row = "<tr class='p-3' style='background-color: red;'><td>"+ hour + AmOrPm +"</td>"+
+//         "<td></td>"+
+//         "<td><i class='fa fa-save'></i></td></tr>";
+//     }
+//     else{
+//         row = "<tr class='p-3' style='background-color: grey;'><td>"+  hour + AmOrPm +"</td>"+
+//         "<td><input type='text' class='inputValue' name='name[]' value=''></input></td>"+
+//         "<td><button type='button' class='btn btn-danger'><i class='fa fa-save'></i></button></td></tr>";
+//     }
+//     $("#timeBlocks tbody").append(row);
+//     }
 
+//     prevValues = JSON.parse(localStorage.getItem('values'));
+//     console.log(prevValues);
+// if(prevValues != null){
+//     for (let i = 0; i < prevValues.length; i++) {
+//         $(".inputValue").val(prevValues[i]);
+//         console.log(prevValues[i]);   
+//     }
+// }
+
+
+// console.log($("#timeBlocks tbody").childern());
+// $("#timeBlocks tbody").on('submit','btn-danger',function (event) {
+//     console.log(event.target);
+// })
+
+function printDailyPlannerData(hour, prevValue) {
+    var dailyPlannerRowEl = $('<tr>');
+  
+    var hourTdEl = $('<td>').addClass('p-2').text(hour);
+  
+    var inputTdEl = $('<td>').addClass('p-2');  
+    
+    var inputDataField = $('<input>').addClass('inputData').val(prevValue);
+    inputTdEl.append(inputDataField);
+
+    var saveTdEl = $('<td>').addClass('p-2');
+  
+    var saveDataBtn = $('<button>').addClass('btn btn-danger save-data-btn');
+    var saveIcon = $('<i>').addClass('fa fa-save');
+    saveDataBtn.append(saveIcon);
+    saveTdEl.append(saveDataBtn);
+  
+    // By listing each `<td>` variable as an argument, each one will be appended in that order
+    dailyPlannerRowEl.append(
+        hourTdEl,
+        inputTdEl,
+        saveTdEl
+    );
+  
+    dailyPlanner.append(dailyPlannerRowEl);
+  }
+
+// function saveLocalStorage(event) {
+//     // console.log(event.target);
+//     var values = $('input[name="name[]"]').map(function(){
+//         return this.value;
+//      }).get();
+//      localStorage.setItem('values',JSON.stringify(values));
+     
+//     //  console.log(localStorage.getItem('values'));
+//   }
+//   $("button").on( "click", saveLocalStorage);
 
 
 
